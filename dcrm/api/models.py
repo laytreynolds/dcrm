3193c88c992e665b9dab3a34237accf94c240e7d
laytreynolds <laytreynolds@hotmail.com>
2023-12-24 23:50:18 +0000

    Customer and Order Models

---
+++ dcrm/api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import random
+
+
+def generate_order_number():
+    while True:
+        order_number = random.randint(100000, 999999)
+        # Check if the generated order number already exists
+        if Order.objects.filter(order_number=order_number).count() == 0:
+            break
+    return order_number
+
+# Create your models here.
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=11)
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    order_number = generate_order_number()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_created_at = models.DateTimeField(auto_now_add=True)
