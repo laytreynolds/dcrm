55720b0870a1d4c90b134735cf46a84255ea61da
Layton Reynolds <laytreynolds@hotmail.com>
2025-02-08 18:34:34 +0000

    Settings files

---
+++ dcrm/settings/dev.py
@@ -0,0 +1,31 @@
+from .base import *
+import environ
+import os
+from pathlib import Path
+
+# Initialize environment variables
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# Determine which .env file to read
+env_file = os.getenv('DJANGO_ENV_FILE', '.env.dev')
+env.read_env(os.path.join(BASE_DIR, env_file))
+
+ALLOWED_HOSTS = ["*"]
+
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG="True"
+
+
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "dcrm",
+        "USER": "postgres",
+        "PASSWORD": "Xok87187",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
